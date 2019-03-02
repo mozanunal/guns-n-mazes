9ca388b085e956304cc3d8e6dae174b67013a6e4
mozanunal <mehmetozanunal@gmail.com>
2019-03-02 23:13:37 +0300

    player man added, fire moved to man class

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const PIXI = require('pixi.js');
 const Mouse = require('pixi.js-mouse');
 //misc
-const keyboard = require('./misc/keyboard');
 const calc = require('./misc/calculation');
 // sprites
-const Fire = require('./src/fire');
 const Man = require('./src/man');
+const PlayerMan = require('./src/playerMan');
 
 var app = new PIXI.Application(800, 600, { backgroundColor: 0x004400 });
 document.body.appendChild(app.view);
@@ -27,20 +26,22 @@ function onAssetsLoaded() {
     var GAME_STATE = GS_LOAD;
 
     // player Man
-    var playerMan = Man(app.screen.width / 2, app.screen.height / 2, 0);
+    var playerMan = PlayerMan(app.screen.width / 2, app.screen.height / 2, 0);
     app.stage.addChild(playerMan);
     GLOBAL_SPRITES.push(playerMan);
 
     var man1 = Man(200, 200, 0);
     app.stage.addChild(man1);
     GLOBAL_SPRITES.push(man1);
+    
+    f = man1.fire();
+    app.stage.addChild(f);
+    GLOBAL_SPRITES.push(f);
 
     GAME_STATE = GS_ACTIVE;
     // Animate the rotation
     app.ticker.add(function (delta) {
         if (GAME_STATE == GS_ACTIVE) {
-            playerMan.rotation = calc.getAngleTo(Mouse.getPosX(), Mouse.getPosY(), playerMan.x, playerMan.y);
-
             GLOBAL_SPRITES.forEach(function (sprite) {
                 sprite.objTick(delta);
 
@@ -50,47 +51,13 @@ function onAssetsLoaded() {
 
     Mouse.events.on('released', null, (buttonCode, event, mouseX, mouseY, mouseOriginX, mouseOriginY, mouseMoveX, mouseMoveY) => {
         console.log(buttonCode, mouseOriginX, mouseOriginY, mouseX, mouseY, mouseMoveX, mouseMoveY);
-        var X = playerMan.x + calc.getAngleX(70, playerMan.rotation + calc.degree2Radian(40));
-        var Y = playerMan.y + calc.getAngleY(70, playerMan.rotation + calc.degree2Radian(40));
-        fire = Fire(X, Y, playerMan.rotation);
+        fire = playerMan.fire();
         app.stage.addChild(fire);
         GLOBAL_SPRITES.push(fire);
 
     });
 
-    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
-    let keyObjectUp = keyboard("ArrowUp");
-    let keyObjectDown = keyboard("ArrowDown");
-    let keyObjectLeft = keyboard("ArrowLeft");
-    let keyObjectRight = keyboard("ArrowRight");
-    var MOVEMENT_VEL = 4;
 
-
-    keyObjectUp.press = () => {
-        playerMan.vy = -MOVEMENT_VEL;
-    };
-    keyObjectDown.press = () => {
-        playerMan.vy = MOVEMENT_VEL;
-    };
-    keyObjectRight.press = () => {
-        playerMan.vx = +MOVEMENT_VEL;
-    };
-    keyObjectLeft.press = () => {
-        playerMan.vx = -MOVEMENT_VEL;
-    };
-
-    keyObjectUp.release = () => {
-        playerMan.vy = 0;
-    };
-    keyObjectDown.release = () => {
-        playerMan.vy = 0;
-    };
-    keyObjectRight.release = () => {
-        playerMan.vx = 0;
-    };
-    keyObjectLeft.release = () => {
-        playerMan.vx = 0;
-    };
 }
 
 
---
+++ src/playerMan.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const Fire = require('./fire');
+const calc = require('../misc/calculation');
+const keyboard = require('../misc/keyboard');
+const Mouse = require('pixi.js-mouse');
+const MOVEMENT_VEL = 4;
+
+function man(x, y, rotation) {
+    var walkFrames = [];
+    var stopFrame;
+    for (var i = 1; i < 4; i++) {
+        walkFrames.push(PIXI.Texture.fromFrame('W' + i + '.png'));
+    }
+    let walk = walkFrames;
+    let stop = [walkFrames[1]];
+    let man = new PIXI.extras.AnimatedSprite(walk, stop);
+
+    man.play();
+    man.x = x;
+    man.y = y;
+    man.vx = 0;
+    man.vy = 0;
+    man.anchor.set(0.5);
+    man.animationSpeed = 0.15;
+    man.play("play");
+
+    man.fire = function () {
+        var X = man.x + calc.getAngleX(70, man.rotation + calc.degree2Radian(40));
+        var Y = man.y + calc.getAngleY(70, man.rotation + calc.degree2Radian(40));
+        return Fire(X, Y, man.rotation);
+    }
+
+    man.objTick = function (delta) {
+        man.rotation = calc.getAngleTo(Mouse.getPosX(), Mouse.getPosY(), man.x, man.y);
+        if (man.vx == 0 && man.vy == 0) {
+            man.gotoAndStop(1);
+        } else {
+            man.play();
+        }
+        man.x += man.vx * delta;
+        man.y += man.vy * delta;
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+    let keyObjectUp = keyboard("ArrowUp");
+    let keyObjectDown = keyboard("ArrowDown");
+    let keyObjectLeft = keyboard("ArrowLeft");
+    let keyObjectRight = keyboard("ArrowRight");
+
+    keyObjectUp.press = () => {
+        man.vy = -MOVEMENT_VEL;
+    };
+    keyObjectDown.press = () => {
+        man.vy = MOVEMENT_VEL;
+    };
+    keyObjectRight.press = () => {
+        man.vx = +MOVEMENT_VEL;
+    };
+    keyObjectLeft.press = () => {
+        man.vx = -MOVEMENT_VEL;
+    };
+
+    keyObjectUp.release = () => {
+        man.vy = 0;
+    };
+    keyObjectDown.release = () => {
+        man.vy = 0;
+    };
+    keyObjectRight.release = () => {
+        man.vx = 0;
+    };
+    keyObjectLeft.release = () => {
+        man.vx = 0;
+    };
+
+    return man;
+
+}
+
+
+module.exports = man;
