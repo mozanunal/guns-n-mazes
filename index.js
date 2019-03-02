b26c496ea63dc2ef8d7594e3744b6313e7daf275
mozanunal <mehmetozanunal@gmail.com>
2019-03-02 22:02:34 +0300

    fire mechanism added

--- index.js
+++ index.js
@@ -1,106 +1,124 @@
 
 const PIXI = require('pixi.js');
+const Mouse = require('pixi.js-mouse');
+//misc
 const keyboard = require('./misc/keyboard');
+const calc = require('./misc/calculation');
+// sprites
+const Fire = require('./src/fire');
 
-var app = new PIXI.Application({backgroundColor : 0x444444});
+var app = new PIXI.Application(800, 600, { backgroundColor: 0x004400 });
 document.body.appendChild(app.view);
-
-let playerMan;
 
 PIXI.loader
     .add('assets/BlueMan/BlueMan.json')
     .load(onAssetsLoaded);
 
 
- function onAssetsLoaded()
- {
-     // create an array of textures from an image path
-     var walkFrames = [];
-     var stopFrame;
-     for (var i = 1; i < 4; i++) {
-         var val = i;
+function onAssetsLoaded() {
+    // Settings
+    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+    // GLOBAL
+    var GLOBAL_SPRITES = []
 
-         // magically works since the spritesheet was loaded with the pixi loader
-         walkFrames.push(PIXI.Texture.fromFrame('W' + val + '.png'));
-     }
-     idleFrame = PIXI.Texture.fromFrame('W2.png');
-     // create an AnimatedSprite (brings back memories from the days of Flash, right ?)
-     let walk = walkFrames;
-     let stop = [walkFrames[1]];
-     //let mc = new AnimatedSprite(walk,stop);
-     
-     
-     //playerMan = new PIXI.extras.AnimatedSprite(walkFrames);
-     playerMan = new PIXI.extras.AnimatedSprite(walk,stop);
+    // player Man
+    var walkFrames = [];
+    var stopFrame;
+    for (var i = 1; i < 4; i++) {
+        var val = i;
 
-     //playerMan = new AnimatedSprite(walk,stop);
-     playerMan.play(0);
-   
-     
-     /*
-     * An AnimatedSprite inherits all the properties of a PIXI sprite
-     * so you can change its position, its anchor, mask it, etc
-     */
-     playerMan.x = app.screen.width / 2;
-     playerMan.y = app.screen.height / 2;
-     playerMan.anchor.set(0.5);
-     playerMan.animationSpeed = 0.15;
-     playerMan.play("play");
+        // magically works since the spritesheet was loaded with the pixi loader
+        walkFrames.push(PIXI.Texture.fromFrame('W' + val + '.png'));
+    }
+    idleFrame = PIXI.Texture.fromFrame('W2.png');
+    let walk = walkFrames;
+    let stop = [walkFrames[1]];
+    //let mc = new AnimatedSprite(walk,stop);
+
+    //playerMan = new PIXI.extras.AnimatedSprite(walkFrames);
+    let playerMan = new PIXI.extras.AnimatedSprite(walk, stop);
+    //playerMan = new AnimatedSprite(walk,stop);
+    playerMan.play(0);
+    playerMan.x = app.screen.width / 2;
+    playerMan.y = app.screen.height / 2;
+    playerMan.vx = 0;
+    playerMan.vy = 0;
+    playerMan.anchor.set(0.5);
+    playerMan.animationSpeed = 0.15;
+    playerMan.play("play");
 
     app.stage.addChild(playerMan);
 
-    playerMan.vx = 0;
-    playerMan.vy = 0;
+    ;
 
-     // Animate the rotation
-     app.ticker.add(function(delta) {
-        if(playerMan.vx==0&&playerMan.vy==0){
-           // playerMan.stop();
-           playerMan.gotoAndStop(1);
-           
+    // Animate the rotation
+    app.ticker.add(function (delta) {
+        if (playerMan.vx == 0 && playerMan.vy == 0) {
+            // playerMan.stop();
+            playerMan.gotoAndStop(1);
+
         } else {
-           // playerMan.play();
-           playerMan.play();
-           playerMan.go
-           
+            // playerMan.play();
+            playerMan.play();
+            playerMan.go
+
         }
-         playerMan.x += playerMan.vx*delta;
-         playerMan.y += playerMan.vy*delta;
-     });
- }
+        playerMan.x += playerMan.vx * delta;
+        playerMan.y += playerMan.vy * delta;
+
+        playerMan.rotation = calc.getAngleTo(Mouse.getPosX(), Mouse.getPosY(), playerMan.x, playerMan.y);
+
+        GLOBAL_SPRITES.forEach(function (sprite) {
+            sprite.objTick(delta);
+        });
+    });
+
+    Mouse.events.on('released', null, (buttonCode, event, mouseX, mouseY, mouseOriginX, mouseOriginY, mouseMoveX, mouseMoveY) => {
+        console.log(buttonCode, mouseOriginX, mouseOriginY, mouseX, mouseY, mouseMoveX, mouseMoveY);
+        var X = playerMan.x + calc.getAngleX(70, playerMan.rotation + calc.degree2Radian(40));
+        var Y = playerMan.y + calc.getAngleY(70, playerMan.rotation + calc.degree2Radian(40));
+        fire = Fire(X, Y, playerMan.rotation);
+        app.stage.addChild(fire);
+        GLOBAL_SPRITES.push(fire);
+
+    });
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+    let keyObjectUp = keyboard("ArrowUp");
+    let keyObjectDown = keyboard("ArrowDown");
+    let keyObjectLeft = keyboard("ArrowLeft");
+    let keyObjectRight = keyboard("ArrowRight");
+    var MOVEMENT_VEL = 4;
+
+
+    keyObjectUp.press = () => {
+        playerMan.vy = -MOVEMENT_VEL;
+    };
+    keyObjectDown.press = () => {
+        playerMan.vy = MOVEMENT_VEL;
+    };
+    keyObjectRight.press = () => {
+        playerMan.vx = +MOVEMENT_VEL;
+    };
+    keyObjectLeft.press = () => {
+        playerMan.vx = -MOVEMENT_VEL;
+    };
+
+    keyObjectUp.release = () => {
+        playerMan.vy = 0;
+    };
+    keyObjectDown.release = () => {
+        playerMan.vy = 0;
+    };
+    keyObjectRight.release = () => {
+        playerMan.vx = 0;
+    };
+    keyObjectLeft.release = () => {
+        playerMan.vx = 0;
+    };
+}
 
 
 
-// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
-let keyObjectUp = keyboard("ArrowUp");
-let keyObjectDown = keyboard("ArrowDown");
-let keyObjectLeft = keyboard("ArrowLeft");
-let keyObjectRight = keyboard("ArrowRight");
-var MOVEMENT_VEL = 4;
 
 
-keyObjectUp.press = () => {
-    playerMan.vy = -MOVEMENT_VEL;
-};
-keyObjectDown.press = () => {
-    playerMan.vy = MOVEMENT_VEL;
-};
-keyObjectRight.press = () => {
-    playerMan.vx = +MOVEMENT_VEL;
-};
-keyObjectLeft.press = () => {
-    playerMan.vx = -MOVEMENT_VEL;
-};
-
-keyObjectUp.release = () => {
-    playerMan.vy = 0;
-};
-keyObjectDown.release = () => {
-    playerMan.vy = 0;
-};
-keyObjectRight.release = () => {
-    playerMan.vx = 0;
-};
-keyObjectLeft.release = () => {
-    playerMan.vx = 0;
-};
---
+++ misc/calculation.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function degree2Radian(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
+
+function getAngleTo(mx, my, px, py) {
+    var self = this;
+    var distX = my - py;
+    var distY = mx - px;
+    var angle = Math.atan2(distX, distY) + Math.PI / 2;
+    //var degrees = angle * 180/ Math.PI;
+    return angle;
+}
+
+function getAngleX(length, angle) {
+    return Math.cos(angle - Math.PI / 2) * length;
+}
+
+function getAngleY(length, angle) {
+    return Math.sin(angle - Math.PI / 2) * length;
+}
+
+module.exports.getAngleTo = getAngleTo;
+module.exports.getAngleX = getAngleX;
+module.exports.getAngleY = getAngleY;
+module.exports.degree2Radian = degree2Radian;
