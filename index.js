6638002791c5308768bc6abf9669eef1d7001a6a
mozanunal <mehmetozan.unal@basestech.com>
2019-03-02 18:05:52 +0300

    merge with animated sprite

--- index.js
+++ index.js
@@ -1,75 +1,85 @@
 
 const PIXI = require('pixi.js');
+const keyboard = require('./misc/keyboard');
 
-var app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+var app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-// create a new Sprite from an image path
+ let playerMan;
 
-var deneme = PIXI.Sprite.fromImage('assets/cat/cat.png');
-
-
-function Bunny() {
-    var bunny = PIXI.Sprite.fromImage('assets/cat/cat.png');
-    // center the sprite's anchor point
-    bunny.anchor.set(0.5);
-    // move the sprite to the center of the screen
-    bunny.x = app.screen.width / 2;
-    bunny.y = app.screen.height / 2;
-    bunny.vx = 0;
-    bunny.vy = 0;
-    return bunny;
-}
-
-bun1 = Bunny();
+ PIXI.loader
+     .add('assets/BlueMan/BlueMan.json')
+     .load(onAssetsLoaded);
 
 
+ function onAssetsLoaded()
+ {
+     // create an array of textures from an image path
+     var frames = [];
 
-app.stage.addChild(bun1);
+     for (var i = 1; i < 4; i++) {
+         var val = i;
 
-// Listen for animate update
-app.ticker.add(function (delta) {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bun1.rotation += 0.1 * delta;
-    bun1.x += bun1.vx;
-    bun1.y += bun1.vy;
+         // magically works since the spritesheet was loaded with the pixi loader
+         frames.push(PIXI.Texture.fromFrame('W' + val + '.png'));
+     }
 
-});
+     // create an AnimatedSprite (brings back memories from the days of Flash, right ?)
+     playerMan = new PIXI.extras.AnimatedSprite(frames);
+     /*
+     * An AnimatedSprite inherits all the properties of a PIXI sprite
+     * so you can change its position, its anchor, mask it, etc
+     */
+     playerMan.x = app.screen.width / 2;
+     playerMan.y = app.screen.height / 2;
+     playerMan.anchor.set(0.5);
+     playerMan.animationSpeed = 0.1;
+     playerMan.play();
+
+     app.stage.addChild(playerMan);
+
+     playerMan.vx = 0;
+     playerMan.vy=0;
+
+     // Animate the rotation
+     app.ticker.add(function(delta) {
+         playerMan.x += playerMan.vx*10*delta;
+         playerMan.y += playerMan.vy*10*delta;
+     });
+ }
 
 
-// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
-let keyObjectUp = keyboard("ArrowUp");
-let keyObjectDown = keyboard("ArrowDown");
-let keyObjectLeft = keyboard("ArrowLeft");
-let keyObjectRight = keyboard("ArrowRight");
-var MOVEMENT_VEL = 1;
+
+ // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+ let keyObjectUp = keyboard("ArrowUp");
+ let keyObjectDown = keyboard("ArrowDown");
+ let keyObjectLeft = keyboard("ArrowLeft");
+ let keyObjectRight = keyboard("ArrowRight");
+ var MOVEMENT_VEL = 1;
 
 
-keyObjectUp.press = () => {
-    bun1.vy = -MOVEMENT_VEL;
-};
-keyObjectDown.press = () => {
-    bun1.vy = MOVEMENT_VEL;
-};
-keyObjectRight.press = () => {
-    bun1.vx = +MOVEMENT_VEL;
-};
-keyObjectLeft.press = () => {
-    bun1.vx = -MOVEMENT_VEL;
-};
+ keyObjectUp.press = () => {
+     playerMan.vy = -MOVEMENT_VEL;
+ };
+ keyObjectDown.press = () => {
+     playerMan.vy = MOVEMENT_VEL;
+ };
+ keyObjectRight.press = () => {
+     playerMan.vx = +MOVEMENT_VEL;
+ };
+ keyObjectLeft.press = () => {
+     playerMan.vx = -MOVEMENT_VEL;
+ };
 
-keyObjectUp.release = () => {
-    bun1.vy = 0;
-};
-keyObjectDown.release = () => {
-    bun1.vy = 0;
-};
-keyObjectRight.release = () => {
-    bun1.vx = 0;
-};
-keyObjectLeft.release = () => {
-    bun1.vx = 0;
-};
-
+ keyObjectUp.release = () => {
+     playerMan.vy = 0;
+ };
+ keyObjectDown.release = () => {
+     playerMan.vy = 0;
+ };
+ keyObjectRight.release = () => {
+     playerMan.vx = 0;
+ };
+ keyObjectLeft.release = () => {
+     playerMan.vx = 0;
+ };
