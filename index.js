45dd7a1c93c9f8e414b4133c81a9c50645ae3617
mozanunal <mehmetozan.unal@basestech.com>
2019-03-06 00:50:24 +0300

    add sprite to a sprite

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Man = require('./src/man');
 const PlayerMan = require('./src/playerMan');
 const drawMap = require('./src/map0');
 
-var app = new PIXI.Application(1500,700, { backgroundColor: 0x004400 });
+var app = new PIXI.Application(1500, 700, { backgroundColor: 0x004400 });
 document.body.appendChild(app.view);
 
 const blocks = ['assets/BlueMan/Dg1.png', 'assets/BlueMan/Dg2.png', 'assets/BlueMan/Dg3.png', 'assets/BlueMan/Dg4.png', 'assets/BlueMan/Dg5.png'];
@@ -31,8 +31,8 @@ function onAssetsLoaded() {
     var GAME_STATE = GS_LOAD;
 
     // player Man
-   // var playerMan = PlayerMan(app, app.screen.width / 2, app.screen.height / 2, 0);
-    var playerMan = PlayerMan(app, app.screen.width / 2, (app.screen.height / 2)-50, 0);
+    // var playerMan = PlayerMan(app, app.screen.width / 2, app.screen.height / 2, 0);
+    var playerMan = PlayerMan(app, app.screen.width / 2, (app.screen.height / 2) - 50, 0);
 
     app.stage.addChild(playerMan);
     console.log(blocks[0]);
@@ -44,27 +44,51 @@ function onAssetsLoaded() {
     f = man1.fire();
 
     app.stage.position.set(app.screen.width / 2, app.screen.height / 2);
-    app.stage.scale.set(0.75,0.75);
+    app.stage.scale.set(0.75, 0.75);
     app.stage.pivot.copy(playerMan.position);
     GAME_STATE = GS_ACTIVE;
+
+    const HB_LEN = 128;
+    const HB_THICKNESS = 12;
+    //Create the health bar
+    healthBar = new PIXI.Container();
+    healthBar.position.set(-HB_LEN/2, 60)
+    playerMan.addChild(healthBar);
+
+    //Create the black background rectangle
+    let innerBar = new PIXI.Graphics();
+    innerBar.beginFill(0x000000);
+    innerBar.drawRect(0, 0, HB_LEN, HB_THICKNESS);
+    innerBar.endFill();
+    healthBar.addChild(innerBar);
+
+    //Create the front red rectangle
+    let outerBar = new PIXI.Graphics();
+    outerBar.beginFill(0xFF3300);
+    outerBar.drawRect(0, 0, HB_LEN, HB_THICKNESS);
+    outerBar.endFill();
+    healthBar.addChild(outerBar);
+
+    healthBar.outer = outerBar;
+
     // Animate the rotation
     app.ticker.add(function (delta) {
         if (GAME_STATE == GS_ACTIVE) {
-            var t1 = performance.now; 
+            var t1 = performance.now;
             app.stage.children.forEach(function (sprite) {
-                if (sprite.objTick != undefined){
+                if (sprite.objTick != undefined) {
                     sprite.objTick(delta);
                 }
-                
+
             });
-            var t2 = performance.now; 
+            var t2 = performance.now;
             //console.log("One tick", (t2-t1));
         }
         //playerMan.objCollider();
         app.stage.pivot.copy(playerMan.position);
     });
 
-    
+
 
 
 }
